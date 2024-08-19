43b9e5e0b91ecea89f088c239b0e592d543609a8
Alex Hayward <alex.hayward@telespazio.com>
2024-08-19 16:26:44 +0100

    Fix function name

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     setup_requires=["wheel"],
     zip_safe=False,
     license="BSD License",
-    packages=find_packages(exclude=["tests*"]),
+    packages=find_namespace_packages(exclude=["tests*"]),
     include_package_data=True,
     package_data={},
     classifiers=[
